16b9fe5b46ac2fb82f1d83b40d633491e3390d45
Joshua Kern <joshuakerndev@gmail.com>
2021-06-18 01:43:05 -0700

    Create basic layouts of About and Services pages, create Service page circles and add state for Current Circle

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -4,7 +4,12 @@ import './AboutPage.scss';
 const AboutPage = () => {
     return (
         <div className="AboutPage">
+            <div className="AboutPageBGSection" />
+            <div className="AboutPageBGFilterSection" />
+            <div className="Shadow" />
             
+            <div className="AboutPageCenterButton">Defining the Modern Touch</div>
+
         </div>
     )
 }
--- src/Components/ServicesPage/ServicesPage.jsx
+++ src/Components/ServicesPage/ServicesPage.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ServicesPage.scss';
 
 const ServicesPage = () => {
+
+    const [currentCircle, setCurrentCircle] = useState(1);
+    
+    const handleClickCircle = () => {
+        alert('CLICK');
+        setCurrentCircle(currentCircle + 1);
+    }
+
     return (
         <div className="ServicesPage">
-            
+            <div className="ServicesPageBGSection" />
+            <div className="ServicesPageBGFilterSection" />
+            <div className="Shadow" />
+            <div className="ServicesPageContent">
+                <div className="CircleSection">
+                    <div className={
+                        currentCircle === 1 ? 
+                        "Circle Circle1 CurrentCircle" : 
+                        "Circle Circle1"
+                        }
+                        onClick={
+                            currentCircle === 1 ? handleClickCircle
+                            : ''
+                        } 
+                    />
+                    <p className="CircleText">Hello</p>
+                </div>
+                <div className="CircleSection">
+                    <div className={
+                        currentCircle === 2 ? 
+                        "Circle Circle2 CurrentCircle" : 
+                        "Circle Circle2"
+                        }
+                        onClick={
+                            currentCircle === 2 ? handleClickCircle
+                            : ''
+                        } 
+                    />
+                    <p className="CircleText">Hello</p>
+                </div>
+                <div className="CircleSection">
+                    <div className={
+                        currentCircle === 3 ? 
+                        "Circle Circle3 CurrentCircle" : 
+                        "Circle Circle3"
+                        }
+                        onClick={
+                            currentCircle === 3 ? handleClickCircle
+                            : ''
+                        } 
+                    />
+                    <p className="CircleText">Hello</p>
+                </div>
+                <div className="CircleSection">
+                    <div className={
+                        currentCircle === 4 ? 
+                        "Circle Circle4 CurrentCircle" : 
+                        "Circle Circle4"
+                        }
+                        onClick={
+                            currentCircle === 4 ? handleClickCircle
+                            : ''
+                        } 
+                    />
+                    <p className="CircleText">Hello</p>
+                </div>
+            </div>
         </div>
     )
 }
