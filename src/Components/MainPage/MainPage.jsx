242266bb6c59f32b229f490d6a43dc151ac2764d
Joshua Kern <joshuakerndev@gmail.com>
2021-07-07 23:55:51 -0700

    fix styling for main page

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -4,14 +4,13 @@ import "./MainPage.scss";
 export default function MainPage() {
     return (
         <div className="MainPage">
-            <div className="MainPageBGSection" />
-            <div className="MenuChoicesGroup">
-            </div>
-            <div className="MainPageBottomText">
-                <span>Real Estate Management</span>
-                <p>
-                    Defining the Modern Touch
-                </p>
+            <div className="MainPageBGSection">
+                <div className="MainPageBottomText">
+                    <span>Real Estate Management</span>
+                    <p>
+                        Defining the Modern Touch
+                    </p>
+                </div>
             </div>
         </div>
     )
